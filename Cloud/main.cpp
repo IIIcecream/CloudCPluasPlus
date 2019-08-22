d939feda00e048fc6c0f3018876542c2736dc3a1
tangw-b <tangw-b@glodon.com>
2019-08-22 09:26:48 +0800

    ppt更新

--- Cloud/main.cpp
+++ Cloud/main.cpp
@@ -3,13 +3,18 @@
 #include "..\classDemo.h"
 #include "..\MemberInitTest.h"
 #include "STLDemo.h"
+#include "STL\VectorDemo.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    VectorDemo oVectorDemo;
+    oVectorDemo.demo();
+
     STLDemo oDemo;
     oDemo.transStringToResult("\"1,2:3,4\",\"5,6:\",\":4\"");
     string s = oDemo.transResultToString();
+
 
 
     initTest();
